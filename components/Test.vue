f3651d30439c64f0d53428fecb8cc9436bda38b9
John Herrin <jherrinva@gmail.com>
2019-02-22 18:18:20 -0500

    Add test page for wireframe emulation attempts

---
+++ components/Test.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-container fluid grid-list-sm>
+        <v-layout row wrap>
+          <v-flex d-flex xs12 order-xs5>
+            <v-layout column>
+              <v-flex d-flex>
+                <v-card color="blue-grey" dark tile flat>
+                  <v-card-text>"hahabluegrey"</v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex d-flex>
+                <v-card color="brown" dark tile flat>
+                  <v-card-text>"brown"</v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex d-flex xs12 sm7>
+            <v-layout row wrap>
+              <v-flex d-flex>
+                <v-card color="indigo lighten-2" dark tile flat>
+                  <v-card-text>"indigo"</v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex d-flex>
+                <v-layout row>
+                  <v-flex
+                    v-for="n in 2"
+                    :key="n"
+                    d-flex
+                  >
+                    <v-card
+                      color="amber lighten-2"
+                      tile
+                      flat
+                    >
+                      <v-card-text>"haha"</v-card-text>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex d-flex xs12 sm2 child-flex>
+            <v-card color="orange lighten-2" tile flat>
+              <v-card-text>"haha"</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex d-flex xs12 sm3>
+            <v-card color="red lighten-2" dark tile flat>
+              <v-card-text>"haha"</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
+  </div>
+</template>
---
+++ layouts/testLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app
+    light
+  >
+    <v-toolbar
+      prominent
+      extended
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+
+      <div>
+        <input v-model="message" placeholder="Username">
+        <input v-model="message" placeholder="Password">
+      </div>
+
+      <div>
+        <v-btn
+          color="secondary"
+          raised
+          nuxt
+          to="/inspire"
+        >
+          Register
+        </v-btn>
+
+        <v-btn
+          color="secondary"
+          raised
+          nuxt
+          to="/inspire"
+        >
+          Login
+        </v-btn>
+      </div>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      title: 'UMW CPSC Internship Board'
+    }
+  }
+}
+</script>

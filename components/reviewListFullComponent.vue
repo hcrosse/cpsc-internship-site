15b9b78ee4fd5d771e8e5f3f2a749b62263ab072
John Herrin <jherrinva@gmail.com>
2019-03-21 14:08:03 -0400

    Add basic review list component to front page

---
+++ components/reviewListFullComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-if="reviewOpened">
+      <singleReview />
+    </div>
+    <div v-else>
+      <reviewList />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import singleReview from '~/components/singleReview.vue'
+import reviewList from '~/components/reviewList.vue'
+
+export default {
+  layout: 'default',
+  components: {
+    singleReview,
+    reviewList
+  },
+  data() {
+    return {
+      reviewOpened: false,
+      prevDisabled: true,
+      nextDisabled: false,
+      reviewIndexClicked: -1
+    }
+  },
+  methods: {
+    openReview(index) {
+      this.reviewOpened = true
+    }
+  }
+}
+</script>
---
+++ components/singleReview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>REVIEW LIST INDEX CLICKED: {{ reviewIndexClicked }}</p>
+    <v-btn
+      @click="exampleMethod()"
+    >
+      BACK
+    </v-btn>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      reviewOpened: true,
+      selected: [2],
+      reviewIndexClicked: this.$parent.reviewIndexClicked
+    }
+  },
+  methods: {
+    exampleMethod(index) {
+      this.$parent.reviewOpened = false
+    }
+  }
+}
+</script>

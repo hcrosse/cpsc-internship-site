b6b42778cfd4126728b4f6078caf42177c58aa73
Clare Arrington <clarearrington@gmail.com>
2019-03-25 21:43:28 -0400

    Attempted to put maps on test page

--- pages/test.vue
+++ pages/test.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="Map" />
   <v-layout
     column
     justify-center
@@ -8,25 +9,46 @@
       xs12
       sm8
       md6
-    >
-      <logo />
-      <vuetify-logo />
-    </v-flex>
+    />
     <test />
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-import Test from '~/components/Test.vue'
+import gmapsInit from '~/utils/google-maps'
 
 export default {
-  layout: 'testLayout',
-  components: {
-    Logo,
-    VuetifyLogo,
-    Test
+  name: 'Map',
+  async mounted() {
+    try {
+      const google = await gmapsInit()
+      const geocoder = new google.maps.Geocoder()
+      const map = new google.maps.Map(this.$el)
+
+      geocoder.geocode({ address: 'Austria' }, (results, status) => {
+        if (status !== 'OK' || !results[0]) {
+          throw new Error(status)
+        }
+
+        map.setCenter(results[0].geometry.location)
+        map.fitBounds(results[0].geometry.viewport)
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 </script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.App {
+  width: 100vw;
+  height: 100vh;
+}
+</style>

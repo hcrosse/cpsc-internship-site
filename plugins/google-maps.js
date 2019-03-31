e276cb16c05f87c6858b1e908e4f66aadc06758e
Clare Arrington <clarearrington@gmail.com>
2019-03-31 17:51:52 -0400

    Added API key through ignored .env file

--- plugins/google-maps.js
+++ plugins/google-maps.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import * as VueGoogleMaps from "google-maps";
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "",
-    libraries: "places"
+    key: process.env.GOOGLE_API_KEY,
+    libraries: 'places'
   }
-});
+})

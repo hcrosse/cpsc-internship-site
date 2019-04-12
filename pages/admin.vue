60d7a34aca74451f5d16c9869ea88f94ef95e052
John Herrin <jherrinva@gmail.com>
2019-04-12 19:14:02 -0400

    Update admin page to use new adminComponents

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,29 +1,62 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <blockquote class="blockquote">
-        &#8220;ADMIN PAGE UNDER CONSTRUCTION&#8221;
-        <footer>
-          <small>
-            <em>&mdash;Development Team</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout child-flex>
+        <v-card>
+          <div class="row">
+            <div class="column pendingList">
+              <p>REVIEWS WAITING FOR APPROVAL</p>
+              <reviewListFullComponent review-status="pending" />
+            </div>
+            <div class="column rejectedList">
+              <p>PREVIOUSLY REJECTED REVIEWS</p>
+              <reviewListFullComponent review-status="rejected" />
+            </div>
+          </div>
+        </v-card>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
+
+import reviewListFullComponent from '~/components/adminComps/adminFullListComponent.vue'
+
 export default {
+  layout: 'default',
+  components: {
+    reviewListFullComponent
+  },
   data() {
     return {
-      placeholderVariable: false
+      testvar: 'testvar text here'
     }
   },
   methods: {
-    pullReviewsToValidate: function () {
-      /* make absolutely sure there is a valid ADMIN session token stored */
+    test: function () {
+      // eslint-disable-next-line no-console
+      console.log('admin test function called, testvar text = ' + this.testvar)
     }
   }
 }
 </script>
+
+<style>
+
+.pendingList {
+  width: 50%;
+  border: 5px solid black;
+}
+
+.rejectedList {
+  width: 50%;
+  border: 5px solid red;
+}
+
+.approvedList {
+  width: 50%;
+  border: 5px solid red;
+}
+
+</style>
